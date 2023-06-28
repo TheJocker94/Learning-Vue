7f01494e6360da405d58629eabf8dca498c645ac
Oronzo Daniele Castellano <ocastell@c1r1p3.42firenze.it>
2023-06-28 19:57:26 +0200

    sickboiEraRohho

--- Vue_no_cli/app.js
+++ Vue_no_cli/app.js
@@ -2,19 +2,39 @@ const app = Vue.createApp({
     // data, functions
     data() {
         return{
+            url : 'https://www.google.com',
             showBooks: true,
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false},
+                { title: 'the final empire', author: 'brandon sanderson', img: 'assets/1.jpg', isFav: true},
+            ],
             libbro :"The libbro",
             author :"Gino Lool",
-            age:    45
+            age:    45,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvents(e, data){
+            console.log(e.type);
+            if(data){
+                console.log(data);
+            }
+        },
+        handleMousemove(e){
+            this.x = e.offsetX
+            this.y = e.offsetY
+        },
+        handleFav(e){
+            console.log(e.type, this.books.isFav);
+            // this.books.isFav = !this.books.isFav
         }
     }
 })
-
-console.log("ARRIVO QUA");
 
 app.mount('#app')
