17b89ff37762e4ca30e1fb4d8e3e5dab1806207f
Dani <danielecastellano94@gmail.com>
2023-06-29 19:57:50 +0200

    Almost finished timer

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,15 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score"/>
+  <!-- <p v-if="showResult">Reaction time : {{ score }} ms</p> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true
+      this.showResult = false
+      this.delay = 2000 + Math.random() * 5000
+    },
+    endGame(reaction) {
+      this.isPlaying = false
+      this.score = reaction
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -20,7 +42,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="result">
+        <p>Reaction time : {{ score }} ms</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: ['score'],
+}
+</script>
+
+<style>
+    .result{
+        width: 400px;
+        border-radius: 20px;
+        background-color: #af9f0f;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
