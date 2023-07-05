cf7631b7831fcace4bca4a08063785a2dc4f0d02
TheJocker94 <danielecastellano94@gmail.com>
2023-07-05 19:39:58 +0200

    fetchedData

---
+++ dojo-blog/src/components/PostList.vue
@@ -0,0 +1,26 @@
+
+<template>
+    <div class="pos-list">
+        <div v-for="post in posts" key="post">
+            <SinglePost :post="post" />
+        </div>
+    </div>
+</template>
+
+<script >
+import { onMounted, onUnmounted, onUpdated } from 'vue';
+import SinglePost from '@/components/SinglePost.vue';
+export default {
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => console.log('mounted'))
+        onUnmounted(() => console.log('unmounted'))
+        onUpdated(() => console.log('updated'))
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -1,18 +1,63 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-if="error">{{error}}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>
+      <p>Loading posts ...</p>
+    </div>
   </div>
 </template>
-
-<script>
+<!-- <input type="text" v-model="search">
+  <p>Search term {{search}}</p>
+  <div v-for="name in matchingNames" :key="name">{{name}} </div>
+  <button @click="handleClick">stop watching</button> -->
+  <!-- Last lesson -->
+  <!-- //not reactive values, so when they change, the view will not update
+    //data () values are reactive, so when they change, the view will update
+    //this.name = "Mario" will not work
+    // When you use ref() you get a reactive object
+    // You have to use .value to access the value and it has to be a const
+    // We only use .value inside the setup() function
+    // We can also import reactive and use it instead of ref
+    // the difference is that reactive is used for objects and ref is used for primitive values
+    // and reactive does not retain its reactivity when you destructure it
+    // just use ref for everything, its better 
+    // watchEffect is a function that will run whenever any reactive value inside of it changes
+    // usually used to get data from database
+    // to stop the watcher, you have to assign it to a variable and then call the variable
+  -->
+  <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '@/components/PostList.vue';
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: "Home",
+    components: { PostList },
+    setup() {
+        const posts = ref([]);
+        const error = ref(null);
+        const load = async() => {
+          try {
+            let data = await fetch('http://localhost:3000/posts')
+            if (!data.ok) {
+              throw Error('no data available')
+            }
+            posts.value = await data.json();
+          } catch (err) {
+            error.value = err.message;
+            console.log(error.value)
+          }
+        }
+        load();
+        return {
+            posts,
+            error
+            
+        };
+    },
+    components: { PostList }
 }
 </script>
